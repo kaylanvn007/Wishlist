3bfaddd041ce8227c908e1518bb3231594a20dd4
Kaylan van Niekerk <kaylanvn007@gmail.com>
2021-02-11 11:30:36 +0200

    Started rendering for an item

--- list.js
+++ list.js
@@ -1,3 +1,16 @@
+//Object for each item in the wishlist.
 function Item (name){
     this.name = name;
 };
+
+//Render
+function renderItem(item){
+    let container = document.querySelector(".container");
+    let cell = document.createElement("div");
+    let title = document.createElement('h2');
+
+    title.createTextNode(item.name);
+
+    cell.appendChild(title);
+    container.appendChild(cell);
+}
